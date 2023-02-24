4417c1e9a3303ad72ff804e8feacf825039b25ad
Etienne Selemani <etienneselemani47@gmail.com>
2023-02-24 12:05:55 +0100

    modification approuved

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -4,8 +4,8 @@ import "./contact.css";
 const Contact = () => {
   return (
     <section className="contact section" id="contact">
-        <h2 className='section__title'>Get in touch</h2>
-        <span className='section__subtitle'>Contact Me</span>
+        <h2 className='section__title'>Entrer en contact</h2>
+        <span className='section__subtitle'>Me Contacter</span>
 
         <div className="contact__container container grid">
             <div className="contact__content">
@@ -16,7 +16,7 @@ const Contact = () => {
                         <i className="bx bx-mail-send contact__card-icon"></i>
 
                         <h3 className="contact__card-title">Email</h3>
-                        <span className="contact__card-data">etienneselemani47@gmail.com</span>
+                        <span className="contact__card-data">user@gmail.com</span>
 
                         <a href="mailto:etienneselemani47@gmail.com" className="contact__button">
                             Write me{" "}
@@ -30,7 +30,7 @@ const Contact = () => {
                         <h3 className="contact__card-title">WhatsApp</h3>
                         <span className="contact__card-data">+243-81-080-44-79</span>
 
-                        <a href="https://api.whatsapp.com/send?phone=62214408789&text=Hello, more information !" className="contact__button">
+                        <a href="https://api.whatsapp.com/send?phone=243810804479&text=Hello, more information !" className="contact__button">
                             Write me{" "}
                         <i className="bx bx-right-arrow-alt
                         contact__button-icon"></i></a>
@@ -64,7 +64,7 @@ const Contact = () => {
                         <input type="email" name="email" className="contact__form-input" placeholder="Inserer votre email"/>
                     </div>
 
-                    <div className="contact__form-div">
+                    <div className="contact__form-div contact__form-area">
                         <label className="contact__form-tag">Projet</label>
                         <textarea name="project" cols="30" rows="10" className="contact__form-input" placeholder="Ecrire votre projet"></textarea>
                     </div>
