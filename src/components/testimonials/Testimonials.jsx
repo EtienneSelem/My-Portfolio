cc205b8d6bc9fc6396aa476751f1762d04bd159a
Etienne Selemani <etienneselemani47@gmail.com>
2023-02-24 08:25:14 +0100

    ajout de fonctionnalités de communication

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,6 +1,6 @@
-import React from 'react';
+import React from "react";
 import "./testimonial.css";
-import { Data } from './Data';
+import { Data } from "./Data";
 // Import Swiper React components
 import { Swiper, SwiperSlide } from "swiper/react";
 
@@ -14,41 +14,40 @@ import { Pagination } from "swiper";
 const Testimonials = () => {
   return (
     <section className="testimonial container section">
-        <h2 className='section__title'>My Clients say</h2>
-        <span className='section__subtitle'>Testimonial</span>
+      <h2 className="section__title">My Clients say</h2>
+      <span className="section__subtitle">Testimonial</span>
 
-        <Swiper className="testimonial__container"
-            loop = {true}
-            grabCursor = {true}
-            spaceBetween={24}
-            pagination={{
-              clickable: true,
-            }}
-            breakpoints={{
-              576: {
-                slidesPerView: 2,
-              },
-              768: {
-                slidesPerView: 2,
-                spaceBetween: 48,
-              },
-              
-            }}
-            modules={[Pagination]}
-        >
-            {Data.map(({id, image, title, description}) => {
-                return(
-                    <SwiperSlide className="testimonial__card" key={id}>
-                        <img src={image} alt="" className="testimonial__img" />
-                        <h3 className="testimonial__name">{title}</h3>
-                        <p className="testimonial__description">{description}</p>
-                    </SwiperSlide>
-                )
-            }
-            )}
-        </Swiper>
+      <Swiper
+        className="testimonial__container"
+        loop={true}
+        grabCursor={true}
+        spaceBetween={24}
+        pagination={{
+          clickable: true,
+        }}
+        breakpoints={{
+          576: {
+            slidesPerView: 2,
+          },
+          768: {
+            slidesPerView: 2,
+            spaceBetween: 48,
+          },
+        }}
+        modules={[Pagination]}
+      >
+        {Data.map(({ id, image, title, description }) => {
+          return (
+            <SwiperSlide className="testimonial__card" key={id}>
+              <img src={image} alt="" className="testimonial__img" />
+              <h3 className="testimonial__name">{title}</h3>
+              <p className="testimonial__description">{description}</p>
+            </SwiperSlide>
+          );
+        })}
+      </Swiper>
     </section>
-  )
-}
+  );
+};
 
-export default Testimonials
+export default Testimonials;
