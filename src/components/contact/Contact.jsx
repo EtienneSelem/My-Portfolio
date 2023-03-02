a90a8bac5d81b246cd24f5ac761cca9be0d8c4af
Etienne Selemani <etienneselemani47@gmail.com>
2023-03-02 19:48:23 +0100

    ajout de la bibliothèque  emailJS

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -52,7 +52,7 @@ const Contact = () => {
                         <h3 className="contact__card-title">Messenger</h3>
                         <span className="contact__card-data">Etienne Selemani</span>
 
-                        <a href="https://m.me/crypticalcoder" className="contact__button">
+                        <a href="https://m.me/etienneselemani" className="contact__button">
                             Write me{" "}
                         <i className="bx bx-right-arrow-alt
                         contact__button-icon"></i></a>
--- src/components/testimonials/Data.jsx
+++ src/components/testimonials/Data.jsx
@@ -7,19 +7,19 @@ export const Data = [
     {
         id: 1,
         image: Image1,
-        title: "Sandrine Mobunge",
-        description: "lorem fizjfunvjvj svisvbquhsvqijnvh",
+        title: "Jean Jacques Mayinza",
+        description: "Il est le rare de ce developpeur qui m a apporté la meilleur de solution dans ma boite KIBOPETO, je vous le recommande pour vos services informatiques",
     },
     {
         id: 2,
         image: Image2,
-        title: "Jean Jacques Mayinza",
-        description: "qfdfiqjdshfuqsduvhb qsvhishviojqnsivjoqisjvo qidnihb sqodvjoij",
+        title: "Sandrine Mobunge",
+        description: "Etienne est le meilleur developpeur nous proposant des solutions high tech qui subliment nos entreprises, je vous le recommande en tout cas",
     },
     {
         id: 3,
         image: Image3,
-        title: "Marcel Senga",
-        description: "qncijqnijqdv qdjhijh qdinhdbhodfjhb zdifvjihfovnijhsd",
+        title: "Lidia Bazodisa",
+        description: "Il est de ces rares developpeurs qui m a donné le gout du developpement informatique",
     }
 ]
